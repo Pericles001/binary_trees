000f3841d47e2439083d85f57e5d76021619f747
pericles001 <periclesadjovi@outlook.fr>
2022-02-16 23:28:33 +0100

    Update file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
