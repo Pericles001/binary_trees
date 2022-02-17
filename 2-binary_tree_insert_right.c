fb44916275ae818a541b5f5f43267deabf8b3162
pericles001 <periclesadjovi@outlook.fr>
2022-02-17 03:35:26 +0100

    Added a new case

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
