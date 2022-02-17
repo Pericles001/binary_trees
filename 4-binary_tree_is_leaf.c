08976f2b4348c5024f67a049b8e0a6cf876e7b3b
pericles001 <periclesadjovi@outlook.fr>
2022-02-17 03:58:35 +0100

    Review code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,8 +11,8 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 
-	if (node == NULL || node->left == NULL || node->right == NULL)
+	if (node == NULL || node->left != NULL || node->right != NULL)
 		return (0);
-	else
-		return (1);
+
+	return (1);
 }
