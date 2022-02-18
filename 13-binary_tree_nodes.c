f7e9b953eefef2b69dcf76fa9bff736cbda7cf3d
pericles001 <periclesadjovi@outlook.fr>
2022-02-18 14:55:37 +0100

    Fix spacing

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  * @tree: Pointer to the root node of the tree to count the number of nodes
