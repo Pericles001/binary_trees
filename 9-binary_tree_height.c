165f27d21db982edc2380a5f88c3734955bcb0c6
pericles001 <periclesadjovi@outlook.fr>
2022-02-18 14:44:02 +0100

    Review style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
 
-	/**
-	 * binary_tree_height - Measures the height of a binary tree
-	 * @tree: Pointer to the root node of the tree to measure the height
-	 *
-	 * Return: 0 if the tree is NULL
-	 *
-	 *
-	 */
+/**
+* binary_tree_height - Measures the height of a binary tree
+* @tree: Pointer to the root node of the tree to measure the height
+*
+* Return: 0 if the tree is NULL
+*
+*
+*/
 
-	size_t
-	binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
