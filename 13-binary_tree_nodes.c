bb3f40b7eefc652d47cee28093b174a934982a23
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-02-18 14:00:04 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,6 @@
  * @tree: Pointer to the root node of the tree to count the number of nodes
  *
  * Return: If the tree is NULL, must return 0
- *
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
