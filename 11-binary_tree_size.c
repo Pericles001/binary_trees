7cab52262e50299658ce00fe8c634aeacccdee71
pericles001 <periclesadjovi@outlook.fr>
2022-02-18 14:51:15 +0100

    Fixed betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - Measures the size of a binary tree
  * @tree: Pointer to the root node of the tree
- * 
+ *
  * Return: If the tree is NULL, must return 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		size += 1 ;
+		size += 1;
 		size += binary_tree_size(tree->left);
 		size += binary_tree_size(tree->right);
 
